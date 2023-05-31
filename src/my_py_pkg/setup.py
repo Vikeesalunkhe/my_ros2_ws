697c85a7674638fab3fdbbe126c5c8f959849725
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-05-31 23:52:53 +0530

    ROS2-node

--- src/my_py_pkg/setup.py
+++ src/my_py_pkg/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            "py_node = my_py_pkg.my_first_node:main"  #add "py_node = your_pkg_name.your_file_node_name:main"
         ],
     },
 )
