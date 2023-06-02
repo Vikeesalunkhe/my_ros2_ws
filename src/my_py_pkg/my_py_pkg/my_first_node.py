6757a73241811a6fc86fcee363b953487240f9c9
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-06-02 09:03:35 +0530

    my_first_node

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -3,12 +3,19 @@
 import rclpy
 from rclpy.node import Node
 
+class MyNode(Node):
+
+    def __init__(self):
+        super().__init__("py_test")
+        self.get_logger().info("Hello ROS2")
+        self.create_timer(0.5,self.timer_callback)
+
+    def timer_callback(self):
+        self.get_logger().info("Hello")
+
 def main(args=None):
     rclpy.init(args=args)
-    node = Node("py_test")
-    node.get_logger().info("Hello ROS2")
-    #node.get_logger().error("Hello ROS2")
-    #node.get_logger().warning("Hello ROS2")
+    node = MyNode()
     rclpy.spin(node)     #program is still running
     rclpy.shutdown()
 
