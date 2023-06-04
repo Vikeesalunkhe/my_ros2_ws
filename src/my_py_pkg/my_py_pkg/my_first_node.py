1fabded186321caee1a7a482e22681c5c8a7a482
Vikeesalunkhe <vikeesalunkhe2002@gmail.com>
2023-06-04 18:21:52 +0530

    first_hello_node_py

--- src/my_py_pkg/my_py_pkg/my_first_node.py
+++ src/my_py_pkg/my_py_pkg/my_first_node.py
@@ -7,11 +7,13 @@ class MyNode(Node):
 
     def __init__(self):
         super().__init__("py_test")
+        self.counter_ = 0
         self.get_logger().info("Hello ROS2")
         self.create_timer(0.5,self.timer_callback)
 
     def timer_callback(self):
-        self.get_logger().info("Hello")
+        self.counter_ += 1
+        self.get_logger().info("Hello" + str(self.counter_))
 
 def main(args=None):
     rclpy.init(args=args)
